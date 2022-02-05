0b9ba93b8a3c4d38b72c5d9f0151383f67d4703e
mt1322 <hrscun2@gmail.com>
2022-02-06 02:08:43 +0900

    update design 2/6

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,79 +3,96 @@
         chat
     </x-slot>
 
-    <h1> Chat </h1>
-    <h2> {{ session('channelName')[$channel] }} </h2>
-    <form method="post" action="{{ route('destroyOwnChannel', $channel) }}" class="delete-channel">
-        @method('DELETE')
-        @csrf
-        <button class="delete-btn"> x </button>
-    </form>
+    <!--<div class="scroll">
+        <div id="scroll-inner">-->
 
-    {{-- <!-- <a href="{{ route('add') }}"> addChannel </a> --> --}}
-    <form method="post" action="{{ route('add') }}">
-        @csrf
-        <input type="text" name="newChannel">
+    <div class="chat-header">
+        <h1> Chat </h1>
+        <h2> {{ session('channelName')[$channel] }} </h2>
+        <form method="post" action="{{ route('destroyOwnChannel', $channel) }}" class="delete-channel">
+            @method('DELETE')
+            @csrf
+            <button class="delete-btn"> x </button>
+        </form>
 
-        <button type="submit">チャンネル作成</button>
-    </form>
+        {{-- <!-- <a href="{{ route('add') }}"> addChannel </a> --> --}}
+        <form method="post" action="{{ route('add') }}">
+            @csrf
+            <input type="text" name="newChannel">
 
-    <br>
+            <button type="submit">チャンネル作成</button>
+        </form>
 
-    @for ($i = 0; $i < $channelNum; $i++)
-        @if ($i !== $channel)
-            <a href="{{ route('change', $i) }}"> {{ session('channelName')[$i] }} </a>
-            <form method="post" action="{{ route('destroyChannel', $i) }}" class="delete-channel">
-                @method('DELETE')
-                @csrf
-                <button class="delete-btn"> x </button>
-            </form>
-        @endif
-    @endfor
+        <br>
 
-    <form method="post" action="{{ route('store') }}">
-        @csrf
-        {{-- <!-- <input type="text" name="user" value="{{ old('', '入力してください') }}"> --> --}}
-        <textarea name="body" value="{{ old('', '入力してください') }}"> </textarea>
-
-        <button type="submit">送信</button>
-    </form>
-
-    <ul>
-        @forelse ($postData as $key => $post)
-            @if ($add_message && $key === count($postData)-1)
-                <li class="message-list new-message">
-            @else
-                <li class="message-list">
+        @for ($i = 0; $i < $channelNum; $i++)
+            @if ($i !== $channel)
+                <a href="{{ route('change', $i) }}"> {{ session('channelName')[$i] }} </a>
+                <form method="post" action="{{ route('destroyChannel', $i) }}" class="delete-channel">
+                    @method('DELETE')
+                    @csrf
+                    <button class="delete-btn"> x </button>
+                </form>
             @endif
-                {{-- {{ var_dump($key); }}
-                {{ var_dump(count($postData)); }} --}}
-                @if ($editNum-1 === $key)
-                    <span class="user">{{ $post->user }}</span> :
-                    <?php /*$qry = $post->id . ',' . $channel;*/ ?>
-                    <form method="post" action="{{ route('update', $post) }}">
-                        @method('PATCH')
-                        @csrf
-                        {{-- <!-- <textarea name="body" class="editArea" value="{{ $post->body }}"></textarea> --> --}}
-                        <input type="text" name="body" class="editArea" value="{{ old('入力してください', $post->body) }}">
-                        <button type="submit">編集</button>
-                    </form>
+        @endfor
+
+        <form method="post" action="{{ route('store') }}">
+            @csrf
+            {{-- <!-- <input type="text" name="user" value="{{ old('', '入力してください') }}"> --> --}}
+            <textarea name="body" value="{{ old('', '入力してください') }}"> </textarea>
+
+            <button type="submit">送信</button>
+        </form>
+
+    </div>
+    <div class="chat-main">
+
+        <ul>
+            @forelse ($postData as $key => $post)
+                @if ($add_message && $key === count($postData)-1)
+                    <li class="message-list new-message">
                 @else
-                    <span class="user">{{ $post->user }}</span> : {{ $post->body }}
-                    <?php /*$qry = $key . ',' . $channel;*/ ?>
-                    <a href="{{ route('edit', $key) }}" class="edit"> edit </a>
+                    <li class="message-list">
                 @endif
+                    {{-- {{ var_dump($key); }}
+                    {{ var_dump(count($postData)); }} --}}
+                    @if ($editNum-1 === $key)
+                        <span class="user">{{ $post->user }}</span> <span class="postBody">:  </span>
+                        <?php /*$qry = $post->id . ',' . $channel;*/ ?>
+                        <form method="post" action="{{ route('update', $post) }}">
+                            @method('PATCH')
+                            @csrf
+                            {{-- <!-- <textarea name="body" class="editArea" value="{{ $post->body }}"></textarea> --> --}}
+                            <input type="text" name="body" class="editArea" value="{{ old('入力してください', $post->body) }}">
+                            <button type="submit" class="edit edit-btn">編集</button>
+                        </form>
+                    @else
+                        <span class="user">{{ $post->user }}</span> </span> <span class="postBody">: {{ $post->body }} </span>
+                        <?php /*$qry = $key . ',' . $channel;*/ ?>
+                        <a href="{{ route('edit', $key) }}" class="edit"> edit </a>
+                    @endif
 
+                    <?php /*$qry = $post->id . ',' . $channel;*/ ?>
+                    <form method="post" action="{{ route('destroy', $post) }}" class="delete-form">
+                        @method('DELETE')
+                        @csrf
+                        <button class="delete-btn"> 削除 </button>
+                    </form>
+                </li>
+            @empty
+                <h2> Not found </h2>
+            @endforelse
+        </ul>
 
-            </li>
-            <?php /*$qry = $post->id . ',' . $channel;*/ ?>
-            <form method="post" action="{{ route('destroy', $post) }}" class="delete-form">
-                @method('DELETE')
-                @csrf
-                <button class="delete-btn"> x </button>
-            </form>
-        @empty
-            <h2> Not found </h2>
-        @endforelse
-    </ul>
+    </div>
+
+{{-- </div>
+</div> --}}
+
+    {{-- <script>
+        let target = document.getElementById('scroll-inner');
+        target.scrollIntoView(false);
+    </script> --}}
 
 </x-layout>
+
