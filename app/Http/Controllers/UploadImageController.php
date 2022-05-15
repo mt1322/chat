f1fd1f9007fcf887be13a03041252cee3447a106
mt1322 <hrscun2@gmail.com>
2022-05-16 01:21:12 +0900

    delete previous image

--- app/Http/Controllers/UploadImageController.php
+++ app/Http/Controllers/UploadImageController.php
@@ -14,22 +14,22 @@ class UploadImageController extends Controller
 	}
 
 	function upload(Request $request){
-        //以前の画像ファイルを削除する
-        $files = Storage::allFiles('public/uploads/');
-        Storage::delete($files);
-
 		$request->validate([
 			'image' => 'required|file|image|mimes:png,jpeg'
 		]);
 		$upload_image = $request->file('image');
 
-		if($upload_image) {
-			//アップロードされた画像を保存する
+		if($upload_image) {                                            //アップロードされた画像を保存する
 			$path = $upload_image->store('uploads', 'public');
-			//画像の保存に成功したらDBに記録する
-			if($path){
+
+			if($path){  			                                   //画像の保存に成功したらDBに記録する
                 $userName = Auth::user()->name;
+
                 if(UploadImage::where('user', $userName)->exists()) {  //既にユーザのアイコンが存在する場合
+                    $file = UploadImage::where('user', $userName)->first();
+                    $filePath = $file->file_path;
+                    Storage::delete("public/" . $filePath);            //以前の画像ファイルを削除する
+
                     UploadImage::where('user', $userName)
                         ->update([
                             'file_name' => $upload_image->getClientOriginalName(),
